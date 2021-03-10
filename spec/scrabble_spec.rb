b478a2a0be7b922c36fc6043919796c7e0c6b3df
Chris Abdo <christophermabdo@icloud.com>
2021-03-10 22:15:45 +0000

    refractor using a before double

--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,13 +1,18 @@
 require 'scrabble'
 
 describe Scrabble do
+    before(:each) do
+        @scrabble = Scrabble.new("")
+      end
     it 'will create a new instance of scrabble' do
-        scrabble = Scrabble.new("")
-        expect(scrabble).to be_instance_of(Scrabble)
+        expect(@scrabble).to be_instance_of(Scrabble)
     end
 
     it 'will respond to a method of score' do
-        scrabble = Scrabble.new("")
-        expect(scrabble).to respond_to(:score)
+        expect(@scrabble).to respond_to(:score)
+    end
+
+    it  'will return the score as an integer' do
+        
     end
 end
